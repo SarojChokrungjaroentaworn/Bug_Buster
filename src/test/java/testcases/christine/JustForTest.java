788ce1bf4aaee9d9246ed936aa31e4b3304aa23a
SarojChokrungjaroentaworn <c-saroj@hotmail.com>
2023-12-19 19:38:30 -0500

    updated

---
+++ src/test/java/testcases/christine/JustForTest.java
@@ -0,0 +1,14 @@
+package testcases.christine;
+
+import org.testng.annotations.Test;
+
+import utilities.CommonMethods;
+
+public class JustForTest extends CommonMethods{
+	
+	@Test
+	public void testOnly() {
+		getDriver();
+	}
+
+}
