2052aa34e83d232855747c7fd45db9ce8f007c81
christineadamss <christineadams.us@gmail.com>
2023-12-21 17:03:55 -0600

    CA_TestCase01
    ContactUsLinkVerification

--- src/test/java/pages/FooterPage.java
+++ src/test/java/pages/FooterPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utilities.BaseClass;
@@ -9,5 +11,7 @@ public class FooterPage {
 	public FooterPage() {
 		PageFactory.initElements(BaseClass.getDriver(), this);
 	}
-
+	
+	@FindBy(xpath = "//div[@class='footer content']/div/div/ul/li[3]/a")
+	public WebElement contactUsLink;
 }
