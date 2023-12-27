aef5c83284b300384e85223b5452d4f228d43260
SarojChokrungjaroentaworn <c-saroj@hotmail.com>
2023-12-27 15:27:23 -0500

    ...

---
+++ src/test/java/testcases/saroj/testRun.java
@@ -0,0 +1,12 @@
+package testcases.saroj;
+
+import org.testng.annotations.Test;
+
+import utilities.CommonMethods;
+
+public class testRun extends CommonMethods{
+@Test
+	public void justForTest() {
+	getDriver();
+}
+}
