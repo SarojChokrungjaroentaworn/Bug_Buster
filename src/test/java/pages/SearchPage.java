766974881dcfec04221c86df90aac7e7e004bb3f
sirirat <siriratsivilai@gmail.com>
2023-12-28 18:16:13 -0500

    updatedSearchPage

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -1,5 +1,8 @@
 package pages;
 
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import utilities.BaseClass;
@@ -8,6 +11,13 @@ public class SearchPage {
 	
 	public SearchPage() {
 		PageFactory.initElements(BaseClass.getDriver(), this);
+		
 	}
+	
+	@FindBy(xpath = "//input[@id='search']")
+	public WebElement searchTextBox;
+	
+	}
+	
+	
 
-}
