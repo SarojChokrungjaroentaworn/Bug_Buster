ed440d043ab4f5ec6b88970a3aca807c6a3db8c7
christineadamss <christineadams.us@gmail.com>
2023-12-28 14:08:55 -0600

    Updated Test Cases 1-3

--- src/test/java/testcases/christinethebaddie/CA_TestCase02.java
+++ src/test/java/testcases/christinethebaddie/CA_TestCase02.java
@@ -1,9 +1,14 @@
 package testcases.christinethebaddie;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import utilities.CommonMethods;
 
 public class CA_TestCase02 extends CommonMethods {
@@ -11,31 +16,40 @@ public class CA_TestCase02 extends CommonMethods {
 	@Test
 	public void subscribeSubmission() {
 		
+		// Initializing the WebDriver
 		getDriver();
+		
+		// Simulating a click on the subscribe link
 		click(fp.subscribeToMailingLink);
 		
+		// Using Assert to ensure the test is executed for the correct page.
 		String expectedUrl = "https://softwaretestingboard.com/subscribe/";
-		String actualUrl = getDriver().getCurrentUrl();
+		String actualUrl = getDriver().getCurrentUrl();	
+		Assert.assertEquals(actualUrl, expectedUrl, "The URL is not correct, back it up!!!");
 		
-		Assert.assertEquals(actualUrl, expectedUrl, "The URL is not correct!!!");
-		
-		sendKey(subp.emailAddressTextBox, getProperty("email"));
+		// Typing into form fields
+		sendKey(subp.emailAddressTextBox, getProperty("email2"));
 		sendKey(subp.firstNameTextBox, getProperty("firstName"));
 		sendKey(subp.lastNameTextBox, getProperty("lastName"));
 		sendKey(subp.companyNameTextBox, getProperty("companyName"));
 		sendKey(subp.positionTextBox, getProperty("Position"));
 		
+		// Simulating clicking on the 'Subscribe' button to submit form
 		click(subp.subscribeButton);
 		
-		
-        WebElement textElement = getDriver().findElement(By.xpath("//div[@id='mce-success-response']"));
+		// I am using Explicit Wait to ensure the element is visible and text is loaded
+		WebDriverWait wait = new WebDriverWait(getDriver(), 20);
+		WebElement textElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='mce-success-response']")));
 
-        String actualText = textElement.getText();
-        String expectedText = "Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you.";
+		// Retrieving the text and storing it in actual text
+		String actualText = textElement.getText();
+		String expectedText = "Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you.";
 
+        // Using Assert to compare outcome and expected
         Assert.assertEquals(actualText, expectedText, "Text verification failed.");
 		
-		
+        // Turring it down
+		getDriver().quit();
 	}
 	
 }
--- src/test/java/testcases/christinethebaddie/CA_TestCase03.java
+++ src/test/java/testcases/christinethebaddie/CA_TestCase03.java
@@ -1,7 +1,42 @@
 package testcases.christinethebaddie;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import utilities.CommonMethods;
 
 public class CA_TestCase03 extends CommonMethods {
 
+	@Test
+	public void requiredFieldAlert() throws InterruptedException  {
+		
+		// Initializing the WebDriver
+		getDriver();
+		
+		// Simulating a click on the subscribe link
+		click(fp.subscribeToMailingLink);
+		
+		// Using Assert to ensure the test is executed for the correct page.
+		String expectedUrl = "https://softwaretestingboard.com/subscribe/";
+		String actualUrl = getDriver().getCurrentUrl();		
+		Assert.assertEquals(actualUrl, expectedUrl, "The URL is not correct, byeee!!!");
+		
+		// Simulating a clicks on the 'Email Address' field and 'Subscribe' button
+		click(subp.emailAddressTextBox);
+		click(subp.subscribeButton);
+		
+		// Locating the alert text by x-path then storing it into textElement to compare by using Assert
+		WebElement textElement = getDriver().findElement(By.xpath("//div[@class='mce_inline_error']"));
+		String actualText = textElement.getText();
+        String expectedText = "This field is required.";
+        Assert.assertEquals(actualText, expectedText, "Text verification failed.");
+		
+        // Just waiting for 3 seconds before quitting - not really necessary
+        Thread.sleep(3000);
+        
+        // Tear it the heck down
+        getDriver().quit();
+	}
 }
